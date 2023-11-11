d44ee969acc48f5cb8da60f3caa6a4437489e3d6
dagokl <dag.b.okland@gmail.com>
2023-11-11 23:01:30 +0100

    Use wandb to track training

--- train.py
+++ train.py
@@ -6,11 +6,19 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
+import wandb
 from config import config
 from dataset import CarControlDataset, get_train_test_car_control_datasets
 from model import SimpleCNN
 
 device = torch.device('cuda:0')
+wandb.init(
+    project='naive-driving-agent-v0',
+    config={
+        'learning_rate': config.get('training.learning_rate'),
+        'batch_size': config.get('training.batch_size'),
+    },
+)
 
 
 def train_batch(
@@ -49,19 +57,28 @@ def test_batch(
 
 
 def main():
-    train_dataset, test_dataset = get_train_test_car_control_datasets(
-        Path('data/first_dataset'), num_train_episodes=120, num_test_episodes=10
-    )
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=2)
-    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True, num_workers=2)
-
+    num_train_episodes = config.get('training.num_train_episodes')
+    num_test_episodes = config.get('training.num_test_episodes')
+    learning_rate = config.get('training.learning_rate')
+    batch_size = config.get('training.batch_size')
+    epochs = config.get('training.epochs')
+    save_path = config.get('training.save_path')
     image_x, image_y = config.get('camera.resolution').values()
+
+    train_dataset, test_dataset = get_train_test_car_control_datasets(
+        Path('data/first_dataset'), num_train_episodes, num_test_episodes
+    )
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
     model = SimpleCNN(image_x, image_y).to(device)
 
     criterion = nn.MSELoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.Adam(model.parameters(), learning_rate)
 
-    for epoch in range(2):
+    wandb.watch(model, criterion, log='all', log_freq=1, log_graph=True)
+
+    for epoch in range(1, epochs + 1):
         print(f'epoch {epoch}')
 
         model.train()
@@ -84,8 +101,8 @@ def main():
         print(f'{train_loss = }')
         print(f'{test_loss = }')
         print()
+        wandb.log({'epoch': epoch, 'train_loss': train_loss, 'test_loss': test_loss})
 
-    save_path = config.get('training.save_path')
     Path(save_path).parent.mkdir(parents=True, exist_ok=True)
     torch.save(model.state_dict(), save_path)
 
