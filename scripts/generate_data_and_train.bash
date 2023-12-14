0406cc626d3a9d5812b18be5d4bcb38e5603c50c
dagokl <dag.b.okland@gmail.com>
2023-12-14 15:46:03 +0100

    Fix oopsie, only killed Carla after datagen fails, not before training

--- scripts/generate_data_and_train.bash
+++ scripts/generate_data_and_train.bash
@@ -16,19 +16,21 @@ while true; do
     echo "Starting data generation script."
     python src/generate_episodes.py
 
+    datagen_exit_status=$?
+
+    # Killing carla
+    pkill -2 CarlaUE4
+    sleep 4
+    pkill -9 CarlaUE4
+    sleep 2
 
     # Check exit status of python script
-    if [ $? -eq 0 ]; then
+    if [ $datagen_exit_status -eq 0 ]; then
         echo "Data generation completed successfully."
         break
     else
         echo "Data generation failed. Will retry until successfull."
     fi
-
-    # Killing carla
-    pkill -2 CarlaUE4
-    sleep 10
-    pkill -9 CarlaUE4
 done
 
 echo "Starting training script."
